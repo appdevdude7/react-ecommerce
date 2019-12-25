d42fd3e40bf52af19fa753ee5a11294add57fd42
appdevdude7 <appdeveloper.akshay@gmail.com>
2019-12-25 13:26:44 -0600

    added header

--- src/App.js
+++ src/App.js
@@ -1,40 +1,24 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/HomePage';
+import Header from './components/header/Header';
 
 import Shop from './pages/shop/Shop';
 
 import {
-  BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <div>
-      <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/shop">Shop</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+    <div>
+      <Header />
       <Switch>
-        <Route path="/shop">
-          <Shop />
-        </Route>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
+        <Route path="/shop" component={Shop} />
+        <Route exact path="/" component={HomePage} />
       </Switch>
-    </Router>
+    </div>
   );
 }
 
